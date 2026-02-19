a6bab6376028d027d20751573a0e3d5d76552314
LordArrin <79581469+LordArrin@users.noreply.github.com>
2026-02-19 10:41:55 +0300

    update

--- dpi_checker.sh
+++ dpi_checker.sh
@@ -4,7 +4,7 @@
 # CONFIGURATION
 # ==============================================================================
 
-USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0"
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0"
 TIMEOUT_TOTAL=12
 TIMEOUT_CONNECT=5
 DEFAULT_THRESHOLD=24576
